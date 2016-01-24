5170aad9ee951ebd3599b7f7ce629186de15573f
Yafeat Alemayhu <irondev@Yafeats-MacBook.local>
2016-01-23 19:07:50 -0500

    Updates
    Bug Fixes/ Vanilla Removal / Update DB

--- vanilla/plugins/Discussants/locale/de.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$Definition['1 discussant'] = '1 Teilnehmer';
-$Definition['%s discussants'] = '%s Teilnehmer';
--- vanilla/plugins/Discussants/locale/en.php
+++
@@ -1,4 +0,0 @@
-<?php if (!defined('APPLICATION')) exit();
-
-$Definition['1 discussant'] = '1 discussant';
-$Definition['%s discussants'] = '%s discussants';
